33126575fd575ad24be9683a0aac5ab4c90c7609
aboongm <aboongm@yahoo.com>
2024-04-24 21:04:26 +0530

    add part1.1

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = ({exercises1, exercises2, exercises3}) => {
+  return (
+    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+  )
+}
+
+export default Total
