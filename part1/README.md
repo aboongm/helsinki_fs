7a94502647c069496e2032288ca54ec0394a8e2e
aboongm <aboongm@yahoo.com>
2024-04-24 21:24:59 +0530

    add pat 1.3

---
+++ part1/README.md
@@ -0,0 +1,6 @@
+## COURSE INFO
+
+courseinfo submission contains
+-   part 1.1
+-   part 1.2
+
--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -5,15 +5,12 @@ const Content = ({
   part1,
   part2,
   part3,
-  exercises1,
-  exercises2,
-  exercises3,
 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
     </div>
   );
 };
