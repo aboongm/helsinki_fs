2e8a8002bb0d90a6bd1b65ca077ea9c6e9dd6a41
aboongm <aboongm@yahoo.com>
2024-04-25 13:39:50 +0530

    add part 1.6 (unicafe step 1)

--- part1/README.md
+++ part1/README.md
@@ -3,4 +3,11 @@
 courseinfo submission contains
 -   part 1.1
 -   part 1.2
+-   part 1.3
+-   part 1.4
+-   part 1.5
 
+
+## UNICAFE
+unicafe submission contains
+-   part 1.6
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
+  return (
+    <main>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+      </div>
+
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </main>
+  )
+}
+
+export default App
