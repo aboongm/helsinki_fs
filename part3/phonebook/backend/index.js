3d249172e9533f8a32cb349bba0c5944233702b4
aboongm <aboongm@yahoo.com>
2024-04-29 16:49:42 +0530

    part 3.6: Phonebook backend step 6

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -72,6 +72,19 @@ app.post("/api/persons", (request, response) => {
   const body = request.body;
 
   console.log('body: ', body);
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+        error: "The name or number is missing"
+    })
+  }
+
+  const existingPerson = persons.find(person => person.name === body.name);
+  if (existingPerson) {
+    return response.status(400).json({
+        error: "name must be unique"
+    });
+  }
 
   const person = {
     id: generateId(),
