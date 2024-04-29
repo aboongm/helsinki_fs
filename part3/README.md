72b9bd314252325bfb22db951ea55a93beabf656
aboongm <aboongm@yahoo.com>
2024-04-29 17:43:22 +0530

    add part 3.9 Phonebook backend step 9

--- part3/README.md
+++ part3/README.md
@@ -10,3 +10,4 @@ phonebook submission contains
 -   part 3.6: Phonebook backend step 6
 -   part 3.7: Phonebook backend step 7
 -   part 3.8*: Phonebook backend step 8
+-   part 3.9 Phonebook backend step 9
--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan")
+const cors = require("cors")
 
 morgan.token("reqBody", (request, response) => {
     if(request.method === "POST") {
@@ -10,6 +11,7 @@ morgan.token("reqBody", (request, response) => {
 })
 
 app.use(express.json());
+app.use(cors())
 // app.use(morgan('tiny'))
 app.use(morgan(
     `:method :url :status :res[content-length] - :response-time ms:reqBody`
@@ -104,7 +106,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
