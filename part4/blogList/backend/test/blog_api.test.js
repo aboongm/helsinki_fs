72c6de1e0e5c6d6320b91d67cd8853f44c5bdfb4
aboongm <aboongm@yahoo.com>
2024-05-01 20:54:29 +0530

    add part 4.9: Blog List Tests, step 2

--- part4/blogList/backend/test/blog_api.test.js
+++ part4/blogList/backend/test/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after } = require('node:test')
+const { test, after, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -6,21 +6,32 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.status, 200)
-    assert.ok(response.headers['content-type'].match(/^application\/json/))
-})
+describe('Blog API', () => {
+    test('blogs are returned as json', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.status, 200)
+        assert.ok(response.headers['content-type'].match(/^application\/json/))
+    })
 
-test('there are two blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, 2)
-})
+    test('there are two blogs', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, 2)
+    })
 
-test('the first blog is about ', async () => {
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(e => e.title)
-  assert.strictEqual(contents.includes('Once Upon a Time'), true)
+    test('the first blog is about ', async () => {
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(e => e.title)
+        assert.strictEqual(contents.includes('Once Upon a Time'), true)
+    })
+
+    test('blogs have id property', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.status, 200)
+        response.body.forEach(blog => {
+            assert.ok(!blog.hasOwnProperty('_id')) 
+            assert.ok(blog.hasOwnProperty('id'))   
+        })
+    })
 })
 
 after(async () => {
--- part4/blogList/backend/utils/logger.js
+++ part4/blogList/backend/utils/logger.js
@@ -1,11 +1,16 @@
 const info = (...params) => {
-    console.log(...params)
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
   }
-  
-  const error = (...params) => {
-    console.error(...params)
+};
+
+const error = (...params) => {
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
   }
-  
-  module.exports = {
-    info, error
-  }
+};
+
+module.exports = {
+  info,
+  error,
+};
