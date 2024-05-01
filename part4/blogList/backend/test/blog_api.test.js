ce1765adea203d35892dd7674dd9f2a7f6cd7c8d
aboongm <aboongm@yahoo.com>
2024-05-01 20:24:21 +0530

    update test

--- part4/blogList/backend/test/blog_api.test.js
+++ part4/blogList/backend/test/blog_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -6,10 +7,9 @@ const app = require('../app')
 const api = supertest(app)
 
 test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.status, 200)
+    assert.strictEqual(response.body.length, 2)
 })
 
 after(async () => {
