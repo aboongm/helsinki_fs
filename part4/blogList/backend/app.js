d599f5884afa8a0220501dee18097983f5b21e4b
aboongm <aboongm@yahoo.com>
2024-05-01 20:09:39 +0530

    update app.js

--- part4/blogList/backend/app.js
+++ part4/blogList/backend/app.js
@@ -1,8 +1,36 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
+const blogsRouter = require("./controllers/blogController");
+const mongoose = require("mongoose");
+
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
 
 app.use(cors())
 app.use(express.json())
+
+
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+
+app.use(middleware.requestLogger);
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
