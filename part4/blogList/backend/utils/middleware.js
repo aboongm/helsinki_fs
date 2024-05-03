2f3718aa3c3e0b0d100f0f594ec51150d0d128a7
aboongm <aboongm@yahoo.com>
2024-05-03 20:24:44 +0530

    update

--- part4/blogList/backend/utils/middleware.js
+++ part4/blogList/backend/utils/middleware.js
@@ -26,6 +26,8 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: "expected `username` to be unique" });
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   next(error);
